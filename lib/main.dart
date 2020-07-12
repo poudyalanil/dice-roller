a492ae5cc386d3ebe2abf2ae19a2fd5f9e650355
Anil Poudyal <anilrunjakote@gmail.com>
2020-07-12 09:40:45 +0545

    UI tweaks

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-backgroundColor: Colors.red,
+        backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text("Dice Roller"),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
-
       ),
     );
   }
 }
-class DicePage extends StatelessWidget{
+
+class DicePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Container();
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
