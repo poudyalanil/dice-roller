6f47cf03624bae035190e9c6266cf1bf82746b54
Anil Poudyal <anilrunjakote@gmail.com>
2020-07-12 08:23:37 +0545

    Assests Management

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text("Dice Roller"),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
 
       ),
     );
+  }
+}
+class DicePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Container();
   }
 }
